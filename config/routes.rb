5d0faccb7db9a10ad45b29d39f437ff67d3ec99d
AlexisCH48 <alexis.concha48@gmail.com>
2021-09-14 20:23:26 -0300

    second_commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'proyects/index'
-  post 'proyects/new', to: 'proyects#new'
+  get 'proyects/new', to: 'proyects#new'
   post 'proyects', to: 'proyects#create'
   root 'proyects#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
